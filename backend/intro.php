a6a4488c676550ae9fd706377d2bc46b2bc4c803
Hari Prasath <Khariprasath351@gmail.com>
2022-07-04 10:47:21 +0530

    Intro to Backend Project

---
+++ backend/intro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    # This is a varible from the forms
+    $name="vinoth";//global
+
+ $Hello ="How R u";
+ 
+
+ function myName(){
+   static $name="Vikram";//local scope
+        global $name;
+    echo $name;
+ }
+echo $name;
+echo "<br>";
+echo $GLOBALS["Hello"];
+?>
+
+    <h1>welcome <?php ECHO myName() . $name; ?></h1>
+</body>
+</html>
+
+
+
+
