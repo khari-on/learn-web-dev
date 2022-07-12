1c4ac5749acf61c936fe481b8690ccaf7eb5be5a
Hari Prasath <Khariprasath351@gmail.com>
2022-07-12 09:59:05 +0530

    php math

---
+++ backend/math.php
@@ -0,0 +1,33 @@
+
+<?php
+//php math();
+// echo(pi());
+// $x = pi();
+// $y= $x * 34;
+
+// echo($x);
+// echo "<br>";
+// echo $y;
+
+
+//min,max
+
+// echo min(7,12,34,56,87,937);
+
+// echo "<br>";
+
+// echo max(7,12,34,56,87,937);
+
+//absolute returns only positive;
+// $d=-789.78;
+
+// echo abs($d) ;
+
+// $x= 64;
+
+// echo sqrt($x);
+
+// echo round(70.4);
+
+// echo rand(10,100);
+?>
