e5a4f02c1d6e133d2cf1b82fb0878682649b29a1
Hari Prasath <Khariprasath351@gmail.com>
2022-07-18 10:23:46 +0530

    operators in php

---
+++ backend/operators.php
@@ -0,0 +1,153 @@
+<?php
+//arithemetic operators: +,-,/,*,%,**
+// echo"Add" .$x+$y;
+// echo "<br>";
+// echo "Sub" .$x-$y;
+// echo "<br>";
+
+// echo"multi :" . $x*$y;
+// echo "<br>";
+
+// echo "Division" .$x/$y;
+// echo "<br>";
+
+// echo "modulus" .$x%$y;
+// echo "<br>";
+
+// echo "expo" .$x**$y;
+// echo "<br>";
+
+// $y=23;
+
+// $x = $y;
+
+// echo $x;
+
+// $x=2;
+// $y=6;
+
+// $x+=$y;
+// echo $x;
+// echo "<br>";
+
+// $x-=$y;
+// echo $x;
+// echo "<br>";
+
+// $x*=$y;
+// echo $x;
+// echo "<br>";
+
+// $x/=$y;
+// echo $x;
+// echo "<br>";
+
+// $x%=$y;
+// echo $x;
+// echo "<br>";
+
+// $x**=$y;
+// echo $x;
+// echo "<br>";
+
+//comparsion oprator.-compare two value return boolean
+
+// $x="2";
+
+// $y=5;
+
+// var_dump($x==$y) ;equal
+// var_dump($x===$y);identical
+
+// var_dump($x!=$y);
+//var_dump($x>=$y);
+// >=,<=
+
+// var_dump($x<>$y)
+
+//<=>
+
+// var_dump($x<=>$y)
+
+
+//increment and decreament:
+//pre increment
+// echo ++$y;
+//pre decrement
+// echo --$y;
+
+// echo $y++;
+// echo "<br>";
+// echo $y;
+// echo $y--;
+// echo "<br>";
+// echo $y;
+// $x=10;
+// $y=13;
+// if(!($x==100)){
+//    echo "hello";
+// }
+// $x="hello";
+// $y="world";
+
+// echo $x.=$y;
+// echo $x;
+
+
+// union
+
+// $x=array(
+//     "a" =>"red",
+//     "b"=> "Green");
+
+//     var_dump($x);
+    // echo "<br>";
+    // echo $x['c'];
+
+    // $x=array("red","Green","Yellow");
+
+    // var_dump($x);
+    // echo "<br>";
+    // // echo $x[1];
+
+    // $y=array(
+    //     "c"=>"Yellow",
+    //     "d"=>"grey");
+
+    //     var_dump($y);
+    // echo "<br>";
+//unioon
+    // $z=$x+$y;
+    // var_dump($z);
+    
+//equality
+
+
+// $x=array(
+//     "a" =>"red",
+//     "b"=> "Green");
+
+//     // var_dump($x);
+
+    
+// $y=array(
+//     "a" =>"re\d",
+//     "b"=> "Green");
+
+//     var_dump($x$y);
+
+//ternary operator
+// $user='';
+
+// $x =(empty($user)) ? "user not found" : "user found ".$user;
+
+
+// echo $x;
+
+//user;
+$y;
+$x= $y ?? "user not found";
+
+echo  $x;
+
+?>
