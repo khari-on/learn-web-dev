4eb711798e2d57ef2bce435e0eed77baf03c6cc0
Hari Prasath <Khariprasath351@gmail.com>
2022-07-19 11:12:11 +0530

    function and array

---
+++ backend/array.php
@@ -0,0 +1,54 @@
+<?php
+
+//indexed array-numberes index starts with0
+
+// $arr = array("bmw",'audi','benz');
+// $count=count($arr);
+// echo "<br>";
+// //accessing array
+// // echo 'my car is '. $arr[2];
+
+// //index is one time s smaller then the count 
+
+// for($ind=0;$ind<$count;$ind++){
+//     echo $arr[$ind];
+//     echo "<br>";
+// }
+
+//associative - named index key valye pair
+
+// $arr = array("a"=>"bmw","b"=>"audi");
+
+// // echo $arr['a'];
+
+// foreach($arr as $key => $value){
+//     echo $key . ' = ' . $value . "<br>";
+// }
+ 
+$cars=array(
+    array("audi",22,2022),
+    array("valvo",52,2032),
+    array("benz",32,2025),
+    array("ferrari",2,2020),
+
+);
+
+// var_dump( $cars[0]);
+// echo "<br>";
+// var_dump ($cars[1]);
+// echo "<br>";
+// var_dump ($cars[2]);
+// echo "<br>";
+// var_dump ($cars[3]);
+// echo "<br>";
+
+for ($row=0; $row<4 ; $row++){
+
+    for($col=0; $col <1;$col++){
+        echo $cars[$row][$col];
+        echo "<br>";
+    }
+
+}
+
+?>
---
+++ backend/function.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+//syntax
+
+//function functionName(){
+    //code to excute
+// }
+// $x="ajith";
+
+// function greets($name,int $age){
+//     echo "welcome ". $name ."age is $age";
+// };
+
+// greets($x,22);
+
+
+// $x=2;
+
+// function add(&$sum){
+// $sum += 5;
+// echo $sum . "<br>";
+// }
+
+// add($x);
+
+
+
+// echo $x;
+
+?>
